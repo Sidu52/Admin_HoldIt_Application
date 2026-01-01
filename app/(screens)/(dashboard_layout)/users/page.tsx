b7119f9dea4c1e8fa2afe8531fe340aae7751154
sidu52 <hitechsidu992@gmail.com>
2026-01-01 18:04:05 +0530

    fix build bug

--- app/(screens)/(dashboard_layout)/users/page.tsx
+++ app/(screens)/(dashboard_layout)/users/page.tsx
@@ -1,45 +1,10 @@
-"use client";
-import {
-  UserFilters,
-  UserTable,
-  UserHeader,
-} from "@/app/components/user_management";
-import { useUsers } from "@/app/hooks/useUserManager";
-import { useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "@/app/store/hooks";
-import { setUsers } from "@/app/store/slices/usersSlice";
-import UserSkeleton from "@/app/loading/user/UserSkeleton";
+import { Suspense } from "react";
+import UserClient from "./UserClient";
 
-export default function Home() {
-  const dispatch = useAppDispatch();
-  const { search, status, pagination } = useAppSelector((state) => state.users);
-  const {
-    data: userData,
-    isLoading: userLoading,
-    error: userError,
-  } = useUsers({
-    page: pagination.currentPage,
-    limit: pagination.itemsPerPage,
-    status,
-    search,
-  });
-
-  // Dispatch actions on data updates
-  useEffect(() => {
-    if (userData) {
-      dispatch(setUsers(userData.data.data));
-    }
-  }, [userData, dispatch]);
-
-  if (userLoading) return <UserSkeleton />;
-
+export default function SignupPage() {
   return (
-    <main className="flex-1 flex flex-col items-center py-6 px-4 md:px-8">
-      <div className="w-full max-w-7xl flex flex-col gap-6">
-        <UserHeader />
-        <UserFilters />
-        <UserTable />
-      </div>
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <UserClient />
+    </Suspense>
   );
 }
